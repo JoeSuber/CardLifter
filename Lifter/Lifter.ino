5478a226fde4d7bcd9567034b57ad0d210a9004b
joseph suber <joesuber@gmail.com>
2015-03-17 22:48:12 -0500

    change notifications to serial monitor

--- Lifter/Lifter.ino
+++ Lifter/Lifter.ino
@@ -278,7 +278,7 @@ void fanDown(byte suck, byte drop, long int dropZone) {
     if (DEBUG)
       Serial.println("go to resting spot: " + String(restServoPos));
     Serial.println("READY");
-    Serial.println("at " + String(stepperArm.currentPosition());
+    Serial.println("at " + String(stepperArm.currentPosition()));
   }
 }
 
