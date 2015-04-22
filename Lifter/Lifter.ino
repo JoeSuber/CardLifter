d144b709ef35b16ea5de1ef02e7912810ced6eed
joseph suber <joesuber@gmail.com>
2015-04-22 01:16:06 -0500

    adding I2c comms to slave and report from slave

--- Lifter/Lifter.ino
+++ Lifter/Lifter.ino
@@ -1,5 +1,6 @@
 #include <AccelStepper.h>
 #include <Servo.h>
+#include <Wire.h>
 
 // #defines are macros for faster pin access than built-in digitalWrite().
 //  *http://masteringarduino.blogspot.com/2013/10/fastest-and-smallest-digitalread-and.html
@@ -24,11 +25,12 @@
 
 boolean DEBUG = false;                      // send stuff to serial monitor for testing
 
+const byte slaveAdr = 0x002;                // I2c "Wire" address for slave mcu 
 const byte ledPin = 13;                     // on board LED
-const byte holdPinArm = 12;                 // enable pins for stepper drivers
+const byte holdPinArm = 12;                 // enable pins for stepper drivers: arm, lift
 const byte holdPinLift = 11;                // HIGH->disable, LOW->enables
 const byte SERV1PIN = 10;                   // servo control pin
-const byte PINST1_ST = 6;          
+const byte PINST1_ST = 6;                   // step & direction pins going to driver boards
 const byte PINST1_DIR = 7;
 const byte PINST2_ST = 4;
 const byte PINST2_DIR = 5;
@@ -41,14 +43,16 @@ const byte PROX_PIN = A0;                   // Analog prox. sensor Pin for card-
 const byte CARD_PIN = A1;                   // Analog prox. for 'card on-board'
 const byte flutterConst = 36;               // analog sensors must change more than this to update serial monitor
 const int cardThickness = 52;               // calculated 141 steps to lift a card. 608steps/turn
-                                            // (20 turns per 86 cards, 141.3953 ...
+                                            // (20 turns per 86 cards, 141.3953 
+const byte i2cSDA = A4;                      // i2c serial bus pins
+const byte i2cSCL = A5;
 const int proxSense2 = 620;                 // threshold for OPB606A showing a card riding on fan housing
 const int proxSense3 = 900;                 // below this a passenger card is still engaged
 const int liftSenseTop = 740;               // above threshold nothing is close to face of OPB606A
 const int HOLDms = 800;                     // this long to get to extremes of servo travel, then relax
-const long int hoverArmPos = 15;            // pick up postion after zeroing against armLimit
-const long int firstArmPos = 120;           // experimentally determined drop-off position
-const long int secondArmPos = 220;          // 2nd drop (less frequent) and max Arm travel
+const long hoverArmPos = 15;            // pick up postion after zeroing against armLimit
+const long firstArmPos = 120;           // experimentally determined drop-off position
+const long secondArmPos = 220;          // 2nd drop (less frequent) and max Arm travel
 const byte closeAng = 115;                  // servo position: closest approach to card-stack
 const byte openAng = 158;                   // servo postion:
 const byte bumpBinSteps = 36;               // for jiggle when grabTryCount goes up.
@@ -58,26 +62,29 @@ int newAnalog1 = 0;                         // hold value from the PROX_PIN sens
 int newAnalog2 = 0;                         // hold value from the CARD_PIN sensor
 int oldAnalog1 = 0;                         // discover changes in the PROX_PIN sensor
 int oldAnalog2 = 0;                         // discover changes in the CARD_PIN sensor
-long int destinationArmPos = firstArmPos;   // arm drop-off point (first or second)
+long destinationArmPos = firstArmPos;   // arm drop-off point (first or second)
 byte grabTryCount = 0;                      // bump the bin to help the grabber
 byte oldServoPos = servoPosition;           // for tracking changes is servo. Are they continuous?
-String inputString = "";
+String inputString = "";                    // serial communication string
 boolean LOADED = false;
 boolean BACKINGOFF = false;
-byte restServoPos = servoPosition;      // used for moving servo back off its maximum travel on either end;
-                                        //set in fanDown()
-boolean stressedServo = false;          // flag for above
-unsigned long timeStart = 0;
+byte restServoPos = servoPosition;          // used for moving servo back off its maximum travel on either end;
+                                            //set in fanDown()
+boolean stressedServo = false;              // flag for above
+unsigned long timeStart = 0;                
 unsigned long timeCurrent = 0;
-boolean UPDATE = true;                  // flag for printing updated info to serial monitor
-unsigned long currentTime = millis();   // I can't seem to declare local variables
-byte currentServoPos = 0;              
-
+boolean UPDATE = true;                      // flag for printing updated info to serial monitor
+unsigned long currentTime = millis();       // I can't seem to declare local variables
+byte currentServoPos = 0;                   
+String commandChar = "";                      // single letter command parsed from Serial input
+long destInt = 0;                       // serial connection sends stepper position for slider carriage as long - this is sent via I2C to slave mcu
+boolean cmdGood = false;                    // most recent I2C command will be executed?
+long movingToPos = 0;                   // report via I2C of where the slave is moving
 AccelStepper stepperArm(AccelStepper::DRIVER, PINST1_ST, PINST1_DIR);
 AccelStepper stepperLift(AccelStepper::DRIVER, PINST2_ST, PINST2_DIR);
 
 const long int liftUpToTop = -200000;     // arbitrary rise until limit switch hit.
-long int liftBackOff = 500;              // steps to back off the switch; zero is limit-switch position
+long liftBackOff = 500;               // steps to back off the switch; zero is limit-switch position
 
 Servo myserv;
 
@@ -86,7 +93,8 @@ volatile byte armState = LOW;
 
 
 void setup()
-{  
+{
+    Wire.begin();  
     myserv.attach(SERV1PIN); 
     myserv.write(servoPosition);
     
@@ -103,7 +111,7 @@ void setup()
     pinAsInput(PROX_PIN);
     
     attachInterrupt(0, limitLift, CHANGE);          // int.0 on pin 2
-    attachInterrupt(1, limitArm, CHANGE);          // int.1 on pin 3
+    attachInterrupt(1, limitArm, CHANGE);           // int.1 on pin 3
     
     digitalLow(holdPinArm);                 // HIGH disables driver, LOW enables
     digitalLow(holdPinLift);                // HIGH disables driver, LOW enables
@@ -206,33 +214,33 @@ void loop() {
     // first character sent switches us into LOADED mode
     // 97="a", 98="b", 99="c", 100="d", 120="x"
     if (Serial.available() > 0) {        
-      inputString = String(Serial.read());
+      commandChar, destInt = parseSerial();
       if (!LOADED) {
         LOADED = true;
         stepperLift.setCurrentPosition(0);
         UPDATE = true;
       }        
       Serial.println("Rx: " + inputString);
-      if (inputString == "97") {              // 'a' hover over input stack
+      if (commandChar == "a") {              // 'a' hover over input stack
         stepperArm.moveTo(hoverArmPos);
         stepperLift.moveTo(stepperLift.currentPosition() - 20);          
       }
-      else if (inputString == "98") {        // 'b' ID'd pile
+      else if (commandChar == "b") {        // 'b' ID'd pile
         stepperArm.moveTo(firstArmPos);
         destinationArmPos = firstArmPos;
       }
-      else if (inputString == "99") {        // 'c' unidentified pile
+      else if (commandChar == "c") {        // 'c' unidentified pile
         stepperArm.moveTo(secondArmPos);
         destinationArmPos = secondArmPos;
       }
-      else if (inputString == "100") {       // 'd' Reset into loading mode until another character sent
+      else if (commandChar == "d") {       // 'd' Reset into loading mode until another character sent
         stepperArm.moveTo(firstArmPos);
         destinationArmPos = firstArmPos;
         LOADED = false;
       }
-      else if (inputString == "120") {
-        DEBUG = !DEBUG;
-        Serial.println("DEBUG <x> is: " + String(DEBUG));
+      else if (commandChar == "x") {
+        cmdGood, movingToPos = sendtoSlave(destInt);
+        Serial.println("slave Mcu sent: " + String(destInt));
       }
       inputString = "";
     }    
@@ -246,6 +254,65 @@ void limitLift() {
 // interrupt function on pin 3, int.1
 void limitArm() {
   armState = digitalState(LIMIT_ARM);
+}
+
+
+long sendtoSlave(long int destiny) {
+  long int accumulator = 0;
+  boolean readyNow = true;
+  Wire.requestFrom(slaveAdr, 6);    // request 6 bytes from slave device #2
+  unsigned long int markstart = millis();
+  // pause for slave to make Wire.available() == true, but time-out if none
+  while (!Wire.available() && readyNow){
+    if ((millis() - markstart) > 700){
+      Serial.println("ERROR in I2C slave");
+      Serial.println("timeout at: " + String((millis() - markstart)) + " ms");
+      readyNow = false;
+    }
+  }
+  // initial state indicator characters r & d
+  Serial.println("from request to ready-to-send: " + String((millis() - markstart)) + " ms");
+  if (readyNow) {
+    readyNow = false;
+    char c = Wire.read();
+    if ((Wire.read() == 'd') && (c == 'r'))
+        readyNow = true;
+  }
+
+  // get the rest of slave's transmission, turn it into longint
+  if ((Wire.available()) && (Wire.available() <= 4){ 
+    accumulator = Wire.read();
+    accumulator |= Wire.read() << 8;
+    accumulator |= Wire.read() << 16;
+    accumulator |= Wire.read() << 24;
+  }
+  
+  // if slave is ready for new destination, transmit to it
+  if (readyNow) { 
+    Wire.beginTransmission(slaveAdr);   // I2c address set globally
+    byte* pInt=(byte*) &destiny;
+    Wire.send(pInt,sizeof(destiny));
+  }
+  readyNess = readyNow;
+  return accumulator;
+}
+
+
+byte, long parseSerial() {
+  // return the waiting contents of the Serial buffer
+  char cmdCode = Serial.read();
+  if (cmdCode == '\n')
+    return 'q', 0 
+  long int bigNum = Serial.parseInt();
+  if ((bigNum == '\n') || (!bignum))
+    return cmdCode, 0
+  if (Serial.peek() != '\n'){
+    Serial.print("Warning: extra commands to process")
+    return parseSerial();
+  }
+  // clear out the remaining newline char
+  Serial.read();      
+  return cmdCode, bigNum;
 }
 
 
